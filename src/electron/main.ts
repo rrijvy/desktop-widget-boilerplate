a9959848815eb590935fb0947d105c0a2232a210
rrijvy <rrijvy@gmail.com>
2025-02-12 17:55:29 +0600

    electron app boilerplate

---
+++ src/electron/main.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { app, BrowserWindow } from "electron";
+import { isDev } from "./utils.js";
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+  if (isDev()) mainWindow.loadURL("http://localhost:5123");
+  else mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+});
